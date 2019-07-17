a9d1d61a06ac6a33c891b0408600b1eed89bfd28
Maksim Artemov <maksim.artemov@jci.com>
2019-07-17 13:37:45 +0100

    Initial Commit

---
+++ BadgedBarButtonItem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BadgedBarButtonItem
+//
+//  Created by Maksim Artemov on 17/07/2019.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
